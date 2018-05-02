a63f8bd6f57c3ee0635bb9129a7838be48d4a351
Johan Kovalsikoski <johank@dbserver.com.br>
2018-05-02 18:08:30 -0300

    Added data to Character model

--- app/src/main/java/com/dbserver/johank/swapi/Character.kt
+++ app/src/main/java/com/dbserver/johank/swapi/Character.kt
@@ -1,3 +1,11 @@
 package com.dbserver.johank.swapi
 
-data class Character (val name: String, val gender: String, val birth_year: String)
+data class Character (
+        val name: String,
+        val height: String,
+        val mass: String,
+        val hair_color: String,
+        val skin_color: String,
+        val eye_color: String,
+        val birth_year: String,
+        val gender: String)
