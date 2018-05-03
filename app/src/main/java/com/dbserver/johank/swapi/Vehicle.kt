6e6036077d2ef148bca75561219852ec46520203
Johan Kovalsikoski <johank@dbserver.com.br>
2018-05-03 15:35:40 -0300

    Added vehicle data class

---
+++ app/src/main/java/com/dbserver/johank/swapi/Vehicle.kt
@@ -0,0 +1,16 @@
+package com.dbserver.johank.swapi
+
+class Vehicle (
+        val name: String,
+        val model: String,
+        val manufacturer: String,
+        val cost_in_credits: String,
+        val lenght: String,
+        val max_atmosphering_speed: String,
+        val crew: String,
+        val passengers: String,
+        val cargo_capacity: String,
+        val consumables: String,
+        val vehicle_class: String,
+        val pilots: List<Character>,
+        val films: List<Film>)
