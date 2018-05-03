ddef128af43d60e595e266215d605b5403f75f93
Johan Kovalsikoski <johank@dbserver.com.br>
2018-05-03 10:01:59 -0300

    Added data to adapter

--- app/src/main/java/com/dbserver/johank/swapi/CharacterAdapter.kt
+++ app/src/main/java/com/dbserver/johank/swapi/CharacterAdapter.kt
@@ -12,8 +12,24 @@ class CharacterAdapter (private val characterList: MutableList<Character>,
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         fun bindView(character : Character){
+
             val nameTextView = itemView.findViewById<TextView>(R.id.nameTextView)
+            val genderTextView = itemView.findViewById<TextView>(R.id.genderTextView)
+            val heightTextView = itemView.findViewById<TextView>(R.id.heightTextView)
+            val massTextView = itemView.findViewById<TextView>(R.id.massTextView)
+            val birthTextView = itemView.findViewById<TextView>(R.id.birthTextView)
+            val hairTextView = itemView.findViewById<TextView>(R.id.hairTextView)
+            val eyeTextView = itemView.findViewById<TextView>(R.id.eyeTextView)
+            val skinTextView = itemView.findViewById<TextView>(R.id.skinTextView)
+
             nameTextView.text = character.name
+            genderTextView.text = character.gender
+            heightTextView.text = if(character.height != "unknown") "${character.height} cm" else character.height
+            massTextView.text = if(character.mass != "unknown") "${character.mass} kg" else character.mass
+            birthTextView.text = character.birth_year
+            hairTextView.text = character.hair_color
+            eyeTextView.text = character.eye_color
+            skinTextView.text = character.skin_color
         }
     }
 
